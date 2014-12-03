770822ba7d3dbefcb1e832dadec40d572c65171f
Eric Abrahamsen <eric@ploonky.com>
2014-12-02 23:21:12 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrintCss::Application.config.secret_key_base = 'f5b63616a69cc6b83415c6cf764cdc36a61ceda26bcc06fdb2d6c6517047aa0a1ca6af6839104fdd331daba97b27a43c31f9a528b88e0a9bcffae5e21d2535e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PrintCss::Application.config.session_store :cookie_store, key: '_print-css_session'
